0ca4c58b5dce6747513df6192218545b67c54896
Travis Bale <travis.bale@gmail.com>
2026-07-20 16:34:03 -0500

    feat(icons): menu, close, trophy icons

---
+++ src/components/icons/TrophyIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" aria-hidden="true">
+    <path d="M7 4V2h10v2h3v3c0 2.21-1.79 4-4 4h-.28c-.62 1.4-1.93 2.44-3.72 2.9V17h3v2H9v-2h3v-3.1c-1.79-.46-3.1-1.5-3.72-2.9H8c-2.21 0-4-1.79-4-4V4h3zm10 5c1.1 0 2-.9 2-2V6h-2v3zM5 6v1c0 1.1.9 2 2 2V6H5z" />
+  </svg>
+</template>
